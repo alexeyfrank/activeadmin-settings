9146b46824f63c4a7274cce20e47acd9b5df7011
Alexey Frank <alexeyfrank@gmail.com>
2013-11-04 14:35:44 +0400

    some fiexs

--- app/controllers/activeadmin_settings/settings_controller.rb
+++ app/controllers/activeadmin_settings/settings_controller.rb
@@ -1,8 +1,8 @@
-class Web::Admin::ActiveadminSettings::SettingsController < ApplicationController
+class ActiveadminSettings::SettingsController < ApplicationController
   # before_filter :authenticate_admin_user!
 
   def update
-    @object = ::ActiveadminSettings::Setting.find(params[:id])
+    @object = ActiveadminSettings::Setting.find(params[:id])
     if @object.update_attributes(params[:setting])
       render :text => @object.value
     else
